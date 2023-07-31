43e4d46081902d7e4636cbbb288f636114a22cb5
alex <alexs123655@gmail.com>
2023-07-31 16:13:32 -0300

    setup mocks

---
+++ src/mocks/index.ts
@@ -0,0 +1,12 @@
+export const Articles = [
+  {
+    title: "Couvert artístico",
+    content: `
+      Muitos restaurantes impõem aos consumidores o pagamento do couvert artístico, valor pago para a apresentação de artistas no estabelecimento. 
+      Porém, os clientes não são obrigados a pagar pelo serviço, principalmente sem aviso prévio, como explica o art. 39 do CDC, que trata sobre práticas abusivas. 
+      Uma delas está no item III: “enviar ou entregar ao consumidor, sem solicitação prévia, qualquer produto, ou fornecer qualquer serviço”. 
+      </br>
+      O mesmo vale para o couvert, que são os petiscos servidos antes do pedido do cliente.
+      Se o estabelecimento servir a comida sem perguntar aos clientes se eles desejam comprar o produto, trata-se de prática abusiva.`,
+  },
+];
