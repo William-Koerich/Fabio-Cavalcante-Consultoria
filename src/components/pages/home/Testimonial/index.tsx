697d5f44f2d70b7f698a0195dd076ca5eb26f7fe
William Koerich <williamkoerich17@gmail.com>
2023-08-02 11:03:59 -0300

    feat: created testimonial

--- src/components/pages/home/Testimonial/index.tsx
+++ src/components/pages/home/Testimonial/index.tsx
@@ -2,14 +2,51 @@ import * as S from "./style"
 
 export function Testimonial () {
     return (
-        <>
-            <S.TestimonialContainer>
-                <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
+    <>
+        <S.TestimonialContainer>
+            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
+            <S.TestimonialContent>
                 <S.TestimonialInfo>
                     <S.TestimonialRole>Manager</S.TestimonialRole>
                     <S.TestimonialName>Lawrence Brown</S.TestimonialName>
                 </S.TestimonialInfo>
-            </S.TestimonialContainer>
-        </>
+                <S.TestimonialText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum sapien accumsan.
+                    Quisque in rhoncus ex, vel pharetra lectus.
+                </S.TestimonialText>
+            </S.TestimonialContent>
+        </S.TestimonialContainer>
+
+        <S.TestimonialContainer>
+            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
+            <S.TestimonialContent>
+                <S.TestimonialInfo>
+                    <S.TestimonialRole>Manager</S.TestimonialRole>
+                    <S.TestimonialName>Lawrence Brown</S.TestimonialName>
+                </S.TestimonialInfo>
+                <S.TestimonialText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum sapien accumsan.
+                    Quisque in rhoncus ex, vel pharetra lectus.
+                </S.TestimonialText>
+            </S.TestimonialContent>
+        </S.TestimonialContainer>
+
+        <S.TestimonialContainer>
+            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
+            <S.TestimonialContent>
+                <S.TestimonialInfo>
+                    <S.TestimonialRole>Manager</S.TestimonialRole>
+                    <S.TestimonialName>Lawrence Brown</S.TestimonialName>
+                </S.TestimonialInfo>
+                <S.TestimonialText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse varius enim in eros elementum sapien accumsan.
+                    Quisque in rhoncus ex, vel pharetra lectus.
+                </S.TestimonialText>
+            </S.TestimonialContent>
+        </S.TestimonialContainer>
+    </>
     )
 }
--- src/components/pages/home/Testimonial/style.ts
+++ src/components/pages/home/Testimonial/style.ts
@@ -4,12 +4,21 @@ export const TestimonialContainer = styled.div`
   display: flex;
   align-items: flex-start;
   gap: 16px;
+  width: 30rem;
+  padding: 4rem 2rem;
+  background-color: var(--background);
 `;
 
 export const TestimonialImage = styled.img`
   width: 80px;
   height: 80px;
   border-radius: 50%;
+`;
+
+export const TestimonialContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
 `;
 
 export const TestimonialInfo = styled.div`
@@ -20,8 +29,20 @@ export const TestimonialInfo = styled.div`
 
 export const TestimonialRole = styled.p`
   font-weight: bold;
+  color: var(--gold);
+  font-size: 16px;
 `;
 
 export const TestimonialName = styled.p`
-  margin-top: 4px;
+  font-size: 18px;
+  font-weight: bold;
+  color: var(--navy-blue);
+`;
+
+export const TestimonialText = styled.p`
+  border-left: 2px solid var(--gold);
+  color: var(--gray-paragraph);
+  padding-left: 1rem;
+  margin-top: 1rem;
+  font-size: 12px;
 `;
