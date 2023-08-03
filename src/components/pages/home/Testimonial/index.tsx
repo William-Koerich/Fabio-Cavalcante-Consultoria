071079d8220afc8702d0e58ae0618b2d41aa75e5
alex <alexs123655@gmail.com>
2023-08-02 21:56:29 -0300

    feat: testimonial area

--- src/components/pages/home/Testimonial/index.tsx
+++ src/components/pages/home/Testimonial/index.tsx
@@ -1,52 +1,95 @@
-import * as S from "./style"
+import * as S from "./style";
+import { Button } from "@/components/ui/Button";
 
-export function Testimonial () {
-    return (
-    <>
+export function Testimonial() {
+  return (
+    <S.Container>
+      <div className="main">
+        <S.LeftContainer></S.LeftContainer>
+        <S.RightContainer>
+          <div className="container">
+            <div className="hero-column">
+              <h6 className="sub-heading">ABOUT US</h6>
+              <h1 className="heading">
+                Liberty consists in the <strong>power of doing</strong> that
+                which is permitted by the law
+              </h1>
+              <p className="paragraph">
+                Integer volutpat suscipit sapien, a iaculis leo lobortis sed.
+                Morbi eu porttitor dolor. Praesent mattis lectus convallis
+                sapien accumsan finibus. Proin dapibus mi dolor, at congue lacus
+                maximus eget. Praesent scelerisque diam est.
+              </p>
+              <p className="small-paragraph">
+                Sed vel erat et enim vestibulum <br />
+                Aliquam at tortor eu dui <br />
+                dignissim Ut dignissim magna vel sagittis
+              </p>
+              <S.ButtonContainer>
+                <Button borderColor="var(--gold)">Get free quote</Button>
+                <Button bgColor="transparent" textColor="var(--navy-blue)">
+                  Read more
+                </Button>
+              </S.ButtonContainer>
+            </div>
+          </div>
+        </S.RightContainer>
+      </div>
+      <S.TestimonialCards>
         <S.TestimonialContainer>
-            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
-            <S.TestimonialContent>
-                <S.TestimonialInfo>
-                    <S.TestimonialRole>Manager</S.TestimonialRole>
-                    <S.TestimonialName>Lawrence Brown</S.TestimonialName>
-                </S.TestimonialInfo>
-                <S.TestimonialText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse varius enim in eros elementum sapien accumsan.
-                    Quisque in rhoncus ex, vel pharetra lectus.
-                </S.TestimonialText>
-            </S.TestimonialContent>
+          <S.TestimonialImage
+            src="/images/testemonial1.png"
+            alt="Testimonial"
+          />
+          <S.TestimonialContent>
+            <S.TestimonialInfo>
+              <S.TestimonialRole>Manager</S.TestimonialRole>
+              <S.TestimonialName>Lawrence Brown</S.TestimonialName>
+            </S.TestimonialInfo>
+            <S.TestimonialText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum sapien accumsan. Quisque
+              in rhoncus ex, vel pharetra lectus.
+            </S.TestimonialText>
+          </S.TestimonialContent>
+        </S.TestimonialContainer>
+
+        <S.TestimonialContainer $bgColor="var(--white)">
+          <S.TestimonialImage
+            src="/images/testemonial1.png"
+            alt="Testimonial"
+          />
+          <S.TestimonialContent>
+            <S.TestimonialInfo>
+              <S.TestimonialRole>Manager</S.TestimonialRole>
+              <S.TestimonialName>Lawrence Brown</S.TestimonialName>
+            </S.TestimonialInfo>
+            <S.TestimonialText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum sapien accumsan. Quisque
+              in rhoncus ex, vel pharetra lectus.
+            </S.TestimonialText>
+          </S.TestimonialContent>
         </S.TestimonialContainer>
 
         <S.TestimonialContainer>
-            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
-            <S.TestimonialContent>
-                <S.TestimonialInfo>
-                    <S.TestimonialRole>Manager</S.TestimonialRole>
-                    <S.TestimonialName>Lawrence Brown</S.TestimonialName>
-                </S.TestimonialInfo>
-                <S.TestimonialText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse varius enim in eros elementum sapien accumsan.
-                    Quisque in rhoncus ex, vel pharetra lectus.
-                </S.TestimonialText>
-            </S.TestimonialContent>
+          <S.TestimonialImage
+            src="/images/testemonial1.png"
+            alt="Testimonial"
+          />
+          <S.TestimonialContent>
+            <S.TestimonialInfo>
+              <S.TestimonialRole>Manager</S.TestimonialRole>
+              <S.TestimonialName>Lawrence Brown</S.TestimonialName>
+            </S.TestimonialInfo>
+            <S.TestimonialText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse varius enim in eros elementum sapien accumsan. Quisque
+              in rhoncus ex, vel pharetra lectus.
+            </S.TestimonialText>
+          </S.TestimonialContent>
         </S.TestimonialContainer>
-
-        <S.TestimonialContainer>
-            <S.TestimonialImage src="/images/testemonial1.png" alt="Testimonial" />
-            <S.TestimonialContent>
-                <S.TestimonialInfo>
-                    <S.TestimonialRole>Manager</S.TestimonialRole>
-                    <S.TestimonialName>Lawrence Brown</S.TestimonialName>
-                </S.TestimonialInfo>
-                <S.TestimonialText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse varius enim in eros elementum sapien accumsan.
-                    Quisque in rhoncus ex, vel pharetra lectus.
-                </S.TestimonialText>
-            </S.TestimonialContent>
-        </S.TestimonialContainer>
-    </>
-    )
+      </S.TestimonialCards>
+    </S.Container>
+  );
 }
--- src/components/pages/home/Testimonial/style.ts
+++ src/components/pages/home/Testimonial/style.ts
@@ -1,12 +1,133 @@
 import styled from "styled-components";
 
-export const TestimonialContainer = styled.div`
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  .main {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  @media screen and (max-width: 968px) {
+    .main {
+      flex-direction: column;
+    }
+  }
+`;
+
+export const RightContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  padding: 3rem 2rem;
+  gap: 2.5rem;
+  .container {
+    width: 100%;
+    .hero-column {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      gap: 1rem;
+      margin-bottom: 7rem;
+
+      .sub-heading {
+        font-size: 1.2rem;
+        font-weight: 700;
+        font-family: "Roboto", sans-serif;
+        color: var(--gold);
+      }
+      .heading,
+      strong {
+        width: 30rem;
+        font-size: 4rem;
+        line-height: 4.5rem;
+        font-weight: 700;
+        font-family: "Roboto", sans-serif;
+        color: var(--navy-blue);
+        strong {
+          color: var(--gold);
+        }
+      }
+      .paragraph,
+      .small-paragraph {
+        font-size: 1.6rem;
+        color: var(--gray-paragraph);
+        font-weight: 400;
+        font-family: "Roboto", sans-serif;
+        margin: 1rem 0 1rem 0;
+      }
+      .small-paragraph {
+        margin-left: 3rem;
+      }
+      @media screen and (min-width: 968px) {
+        .heading {
+          width: 70rem;
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 968px) {
+    width: 100%;
+    display: flex;
+    padding: 2rem;
+    .container {
+      width: 100%;
+    }
+  }
+`;
+export const LeftContainer = styled.div`
+  width: 100%;
+  display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  @media screen and (min-width: 968px) {
+    width: 30rem;
+    justify-content: start;
+    flex-direction: row;
+  }
+`;
+
+export const TestimonialCards = styled.div`
+  max-width: 1140px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  flex: wrap;
+  @media screen and (max-width: 968px) {
+    justify-content: center;
+    flex-direction: column;
+    padding: 2rem;
+  }
+`;
+
+export const TestimonialContainer = styled.div<{ $bgColor?: string }>`
+  width: 100%;
   display: flex;
   align-items: flex-start;
   gap: 16px;
-  width: 30rem;
-  padding: 4rem 2rem;
-  background-color: var(--background);
+  width: 100%;
+  height: 23rem;
+  padding: 6rem 2rem;
+  margin-bottom: -2rem;
+  background-color: ${({ $bgColor }) => $bgColor ?? "var(--background)"};
+  @media screen and (max-width: 968px) {
+    margin: 0;
+    padding: 4rem 2rem;
+  }
 `;
 
 export const TestimonialImage = styled.img`
@@ -16,6 +137,7 @@ export const TestimonialImage = styled.img`
 `;
 
 export const TestimonialContent = styled.div`
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
